08e82021ecc42ba3348eea1fa8f549ea1d2294a8
Mugisha-Elie <iradukunda2k22@gmail.com>
2026-08-06 16:56:58 +0200

    Question 1

---
+++ question1.ts
@@ -0,0 +1,62 @@
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const ConversionRates: Record<string, number> = {
+  usd: 1300,
+  eur: 1420,
+  gbp: 1650
+}
+
+app.get('/convert', (req: Request, res: Response): void => {
+  const { amount, currency } = req.query;
+
+  if (!amount || !currency) {
+    return res.status(400).json({
+      success: false,
+      error: {
+        message: 'amount and currency are required query parameters!'
+      }
+    })
+  }
+
+  const numericAmount = Number(amount);
+  if (isNaN(numericAmount) || numericAmount <= 0) {
+    return res.status(400).json({
+      success: false,
+      error: {
+        message: 'The amount provided must be a valid positive number'
+      }
+    })
+  }
+
+
+  const currencyStr = currency.toString().toLowerCase();
+  const rate = ConversionRates[currencyStr];
+
+  if (!rate) {
+    return res.status(400).json({
+      success: false,
+      error: {
+        message: 'Accepted currency are: usd, eur, gbp. The currency you provided is unsupported'
+      }
+    })
+  }
+
+  const convertedAmount = numericAmount * rate;
+
+  res.status(200).json({
+    success: true,
+    data: {
+      amount: numericAmount,
+      currency: currencyStr,
+      result: convertedAmount
+    }
+  })
+})
+
+
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
